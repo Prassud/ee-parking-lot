06b589810ccec758cd125ccb4f0103d04b9c0b00
Prasanns <prasanna.s@go-jek.com>
2019-07-10 09:42:17 +0530

    Corrects the mains class name and updates parking lot sh script

---
+++ bin/parking_lot.sh
@@ -0,0 +1,9 @@
+BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo $0
+if [ -z "$0" ]
+  then
+     java -jar parking-lot.jar "process_from_CONSOLE"
+     exit 1
+fi
+file="$BIN_DIR/$1"
+java -jar parking-lot.jar "process_from_file" $file
